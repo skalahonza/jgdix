1859b8af5c3724c8171fffdbbaae7e4f453117ae
Jan Skála <j.skala@hotmail.com>
2023-09-30 21:01:36 +0200

    Use jekyll logger

--- _plugins/upcoming_concerts.rb
+++ _plugins/upcoming_concerts.rb
@@ -3,26 +3,26 @@ require 'tzinfo'
 module Jekyll
   module UpcomingConcertsFilter
     def upcoming_concerts(input)
-      # Print the entire input to inspect its structure
-      puts "Input: #{input.inspect}"
+      # Log the entire input to inspect its structure
+      Jekyll.logger.info("UpcomingConcertsFilter:", "Input: #{input.inspect}")
 
       tz = TZInfo::Timezone.get('Europe/Prague')
       today = tz.now.to_date
 
-      # Print today's date to verify if it's correct
-      puts "Today's Date: #{today}"
+      # Log today's date to verify if it's correct
+      Jekyll.logger.info("UpcomingConcertsFilter:", "Today's Date: #{today}")
 
       input.select do |concert|
         begin
           concert_date = Date.parse(concert['date'].to_s)
 
-          # Print each concert date for verification
-          puts "Concert Date: #{concert_date}"
+          # Log each concert date for verification
+          Jekyll.logger.info("UpcomingConcertsFilter:", "Concert Date: #{concert_date}")
 
           concert_date >= today
         rescue => e
-          # Catch any errors in parsing and print them
-          puts "Error parsing concert date: #{e.message}"
+          # Catch any errors in parsing and log them
+          Jekyll.logger.warn("UpcomingConcertsFilter:", "Error parsing concert date: #{e.message}")
           false
         end
       end
