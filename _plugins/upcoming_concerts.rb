25b8c181042c541eb448ce6abb5bc06d210eb4d3
Jan Skála <j.skala@hotmail.com>
2023-09-30 20:57:51 +0200

    Use czech time zone

--- _plugins/upcoming_concerts.rb
+++ _plugins/upcoming_concerts.rb
@@ -1,14 +1,33 @@
+require 'tzinfo'
+
 module Jekyll
-    module UpcomingConcertsFilter
-      def upcoming_concerts(input)
-        today = Date.today
-        input.select do |concert|
+  module UpcomingConcertsFilter
+    def upcoming_concerts(input)
+      # Print the entire input to inspect its structure
+      puts "Input: #{input.inspect}"
+
+      tz = TZInfo::Timezone.get('Europe/Prague')
+      today = tz.now.to_date
+
+      # Print today's date to verify if it's correct
+      puts "Today's Date: #{today}"
+
+      input.select do |concert|
+        begin
           concert_date = Date.parse(concert['date'].to_s)
+
+          # Print each concert date for verification
+          puts "Concert Date: #{concert_date}"
+
           concert_date >= today
+        rescue => e
+          # Catch any errors in parsing and print them
+          puts "Error parsing concert date: #{e.message}"
+          false
         end
       end
     end
   end
-  
-  Liquid::Template.register_filter(Jekyll::UpcomingConcertsFilter)
-  
+end
+
+Liquid::Template.register_filter(Jekyll::UpcomingConcertsFilter)
