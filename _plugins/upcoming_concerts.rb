a026b961ae67fbedbc77626f1c459600aedf58ce
Jan Skála <j.skala@hotmail.com>
2023-10-01 09:21:34 +0200

    Simplify plugin implementation

--- _plugins/upcoming_concerts.rb
+++ _plugins/upcoming_concerts.rb
@@ -1,31 +1,8 @@
-require 'tzinfo'
-
 module Jekyll
   module UpcomingConcertsFilter
     def upcoming_concerts(input)
-      # Log the entire input to inspect its structure
-      Jekyll.logger.info("UpcomingConcertsFilter:", "Input: #{input.inspect}")
-
-      tz = TZInfo::Timezone.get('Europe/Prague')
-      today = tz.now.to_date
-
-      # Log today's date to verify if it's correct
-      Jekyll.logger.info("UpcomingConcertsFilter:", "Today's Date: #{today}")
-
-      input.select do |concert|
-        begin
-          concert_date = Date.parse(concert['date'].to_s)
-
-          # Log each concert date for verification
-          Jekyll.logger.info("UpcomingConcertsFilter:", "Concert Date: #{concert_date}")
-
-          concert_date >= today
-        rescue => e
-          # Catch any errors in parsing and log them
-          Jekyll.logger.warn("UpcomingConcertsFilter:", "Error parsing concert date: #{e.message}")
-          false
-        end
-      end
+      current_date = Date.today
+      input.select { |concert| Date.parse(concert['date'].to_s) >= current_date }
     end
   end
 end
