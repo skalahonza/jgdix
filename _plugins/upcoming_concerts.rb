39b9a35012f2a62ae256ffc767885f82cbf01f62
Jan Skála <j.skala@hotmail.com>
2023-10-01 13:34:28 +0200

    Remove debug logs

--- _plugins/upcoming_concerts.rb
+++ _plugins/upcoming_concerts.rb
@@ -2,8 +2,6 @@ module Jekyll
   module UpcomingConcertsFilter
     def upcoming_concerts(input)
       current_date = Date.today
-      puts current_date
-      Jekyll.logger.info "Today's date is #{current_date}"
       input.select { |concert| Date.parse(concert['date'].to_s) >= current_date }
     end
   end
